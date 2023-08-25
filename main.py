e154a9e7b945281fe93feda95a19b5bf318f563a
Braydan Salinas <bsalinas5@students.avc.edu>
2023-08-25 00:32:11 -0700

    celsius to fahrenheit

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
 def main():
     """
-    ##################################################
-    # Comlete your code here
-    Use the same variables: celsius fahrenheit 
-    ##################################################
+          ##################################################
+          # Comlete your code here
+          Use the same variables: celsius fahrenheit
+          ##################################################
     """
-fahrenheit = 0.0
-
-celsius = float(input("Enter the degrees in celsius."))
-
-fahrenheit = (celsius*(9/5)) +32
-
-"""
-    ########################################
-    # Do not delete the return statement
-    ########################################
-"""
-
-print((f'Fahrenheit:\t{fahrenheit:.2f}'))
-
+    fahrenheit = 0.0
+    celsius = float(input("Enter the degrees in celsius."))
+    fahrenheit = (celsius*(9/5)) +32   
+    print((f'Fahrenheit:\t{fahrenheit:.2f}'))
+    return celsius, fahrenheit
 
 if __name__ == '__main__':
     main()
